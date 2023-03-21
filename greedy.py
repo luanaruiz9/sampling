d95e78049bcc5b7115b8edb899e448c392ea8343
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-03-21 10:06:31 -0400

    Trying gaussians again

--- greedy.py
+++ greedy.py
@@ -77,8 +77,8 @@ def greedy(f, lam, L, k, m): # m is sampling set size
             while s_vec[amax] == 1:
                 amax = idx_x[np.random.choice(n-i)]
         else:
-            #x0 = np.random.multivariate_normal(np.zeros(n-i),np.eye(n-i)/(n-i))
-            x0 = np.ones(n-i)/(n-i)
+            x0 = np.random.multivariate_normal(np.zeros(n-i),np.eye(n-i)/(n-i))
+            #x0 = np.ones(n-i)/(n-i)
             #x0 = np.zeros(n-i)
             #x0[np.random.choice(n-i)]=1
             #res = opt.minimize(f, x0, args=(lam, L, k, s_vec),method='CG',
