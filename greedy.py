94265c9cc9cac139553fe7367a59309bef8aca75
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-03-30 12:56:30 -0400

    Bug fixes

--- greedy.py
+++ greedy.py
@@ -12,30 +12,29 @@ import torch
 
 def f(x, *args):
 
-    x = torch.tensor(x,dtype=torch.float32)
     lam, L, k, s_vec = args
     
     n = L.shape[0]
     # Construct I_vsc from s_vec
-    Ivsc = torch.zeros((n,n-int(np.sum(s_vec))))
+    Ivsc = np.zeros((n,n-int(np.sum(s_vec))))
     # Construct I_scv from s_vec
-    Iscv = torch.zeros((n-int(np.sum(s_vec)),n))
+    Iscv = np.zeros((n-int(np.sum(s_vec)),n))
     idx = np.argwhere(s_vec==0)
     for i in range(n-int(np.sum(s_vec))):
         Ivsc[idx[i],i] = 1
         Iscv[i,idx[i]] = 1
         
-    omega = torch.matmul(Ivsc,x)
+    omega = np.matmul(Ivsc,x)
     for i in range(k):
-        omega = torch.matmul(L,omega)
+        omega = np.matmul(L,omega)
     Lt = torch.t(L)
     for i in range(k):
-        omega = torch.matmul(Lt,omega)
-    omega = torch.matmul(Iscv,omega)
-    omega = torch.matmul(x,omega)
+        omega = np.matmul(Lt,omega)
+    omega = np.matmul(Iscv,omega)
+    omega = np.matmul(x,omega)
      
     #omega = omega.numpy()
-    omega = torch.pow(torch.linalg.norm(omega)/torch.pow(torch.linalg.norm(x),2),1/2*k)
+    omega = np.power(np.linalg.norm(omega)/np.power(np.linalg.norm(x),2),1/2*k)
     
     return lam-omega
 
@@ -51,14 +50,12 @@ def greedy(f, lam, L, k, m, exponent=5): # m is sampling set size
             while s_vec[amax] == 1:
                 amax = idx_x[np.random.choice(n-i)]
         else:
-            x0_array = np.random.multivariate_normal(np.zeros(n-i),np.eye(n-i)/np.sqrt(n-i))
-            x0 = torch.tensor(x0_array, dtype=torch.float32)
+            x0 = np.random.multivariate_normal(np.zeros(n-i),np.eye(n-i)/np.sqrt(n-i))
             #x0 = np.ones(n-i)/(n-i)
             #x0 = np.zeros(n-i)
             #x0[np.random.choice(n-i)]=1
-            k = torch.tensor(k)
-            lam = lam.to('cpu')
-            L = L.to('cpu')
+            lam = lam.cpu().numpy()
+            L = L.cpu().numpy()
             res = opt.minimize(f, x0, args=(lam, L, k, s_vec),method='CG',
                                options={'disp': True})
             #res = opt.minimize(f_lobpcg, np.expand_dims(x0,axis=1), args=(lam, L, k, s_vec),
