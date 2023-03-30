deb65a6736f853d01f8fb8b2437190174615f85a
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-03-30 12:22:15 -0400

    Bug fix

--- main_graphon.py
+++ main_graphon.py
@@ -149,105 +149,6 @@ print()
 ############################# Sampling! ######################################
 ##############################################################################
 
-print('Sampling at random...')
-print()
-
-# Finding sampling set
-m = 100
-n_nodes_per_int, n_nodes_last_int = np.divmod(num_nodes,m)
-graph_ind = generate_induced_graphon(train_data,m)
-num_nodes_ind = graph_ind.x.shape[0]
-edge_index_ind = graph_ind.edge_index
-edge_weight_ind = graph_ind.edge_weight
-adj_sparse_ind = torch.sparse_coo_tensor(edge_index_ind, edge_weight_ind,
-                                         (num_nodes_ind, num_nodes_ind))
-adj_sparse_ind = adj_sparse_ind.to(device)
-adj_ind = adj_sparse_ind.to_dense()
-
-# Computing normalized Laplacian
-L_ind = aux_functions.compute_laplacian(adj_sparse_ind,num_nodes_ind)
-
-lam = eigvals[-1]
-L_aux = L_ind.cpu()
-k = 5
-m2 = 50
-
-s_vec = greedy(f, lam, L_aux, k, m2, exponent=0)
-s_vec = torch.tensor(s_vec)
-
-m3 = 4
-sampled_idx = []
-for i in range(m):
-    if s_vec[i] == 1:
-        if i < m-1:
-            idx = np.random.choice(np.arange(i*n_nodes_per_int,(i+1)*n_nodes_per_int),m3)
-        else:
-            if m3 > n_nodes_last_int:
-                m3 = n_nodes_last_int
-            idx = np.random.choice(np.arange(i*n_nodes_per_int,
-                                             i*n_nodes_per_int+n_nodes_last_int),m3)
-        idx = np.sort(idx)
-        sampled_idx += list(idx)
-
-graph_new = train_data.subgraph(torch.tensor(sampled_idx,device=device,dtype=torch.long))
-graph_new = graph_new.to(device)
-
-num_nodes_new = graph_new.x.shape[0]
-edge_index_new = graph_new.edge_index
-if graph_new.edge_weight is not None:
-    edge_weight_new = graph_new.edge_weight
-else:
-    edge_weight_new = torch.ones(edge_index_new.shape[1]).to(device)
-adj_sparse_new = torch.sparse_coo_tensor(edge_index_new, edge_weight_new, 
-                                         (num_nodes_new, num_nodes_new))
-adj_new = adj_sparse_new.to_dense()
-
-# Computing normalized Laplacian
-L_new = aux_functions.compute_laplacian(adj_sparse_new,num_nodes_new)
-
-K = 5
-eigvals_new, V_new = torch.lobpcg(L_new,k=K)
-V_new = V_new.type(torch.float32)
-V_rec = torch.zeros(num_nodes, K, device=device)
-
-for i in range(V_new.shape[1]):
-    v = V_new[:,i]
-    x0 = np.random.multivariate_normal(np.zeros(num_nodes),np.eye(num_nodes)/np.sqrt(num_nodes))
-    x0[sampled_idx] = v.cpu().numpy()*np.sqrt(m2*m3)/np.sqrt(num_nodes)
-    v_padded_lb = -torch.ones(num_nodes, device=device)
-    v_padded_lb[sampled_idx] = v*np.sqrt(m2*m3)/np.sqrt(num_nodes)
-    v_padded_ub = torch.ones(num_nodes, device=device)
-    v_padded_ub[sampled_idx] = v*np.sqrt(m2*m3)/np.sqrt(num_nodes)    
-    V_rec[:,i] = torch.from_numpy(reconstruct(f_rec, x0, v_padded_lb, v_padded_ub, L, k))
-
-pre_defined_kwargs = {'eigvecs': V_rec}
-
-train_data_new = Data(x=train_data.x, edge_index=train_data.edge_index,
-                      edge_label=train_data.edge_label,
-                      y=train_data.y,edge_label_index=train_data.edge_label_index,
-                      **pre_defined_kwargs)
-val_data_new = Data(x=val_data.x, edge_index=val_data.edge_index,
-                      edge_label=val_data.edge_label,
-                      y=train_data.y,edge_label_index=val_data.edge_label_index,
-                      **pre_defined_kwargs)
-test_data_new = Data(x=test_data.x, edge_index=test_data.edge_index,
-                      edge_label=test_data.edge_label,
-                      y=test_data.y,edge_label_index=test_data.edge_label_index,
-                      **pre_defined_kwargs)
-
-model = SignNetLinkPredNet(dataset.num_features+16*K, 32, 32, True, 1, 16, 16).to(device)
-optimizer = torch.optim.Adam(params=model.parameters(), lr=0.01)
-criterion = torch.nn.BCEWithLogitsLoss()
-model = train_link_predictor(model, train_data_new, val_data_new, optimizer, criterion)
-test_auc = eval_link_predictor(model, test_data_new)
-print(f"Test: {test_auc:.3f}")
-
-print()
-
-##############################################################################
-############################# Sampling! ######################################
-##############################################################################
-
 print('Sampling with spectral proxies...')
 print()
 
@@ -343,3 +244,101 @@ print(f"Test: {test_auc:.3f}")
 
 print()
 
+##############################################################################
+############################# Sampling! ######################################
+##############################################################################
+
+print('Sampling at random...')
+print()
+
+# Finding sampling set
+m = 100
+n_nodes_per_int, n_nodes_last_int = np.divmod(num_nodes,m)
+graph_ind = generate_induced_graphon(train_data,m)
+num_nodes_ind = graph_ind.x.shape[0]
+edge_index_ind = graph_ind.edge_index
+edge_weight_ind = graph_ind.edge_weight
+adj_sparse_ind = torch.sparse_coo_tensor(edge_index_ind, edge_weight_ind,
+                                         (num_nodes_ind, num_nodes_ind))
+adj_sparse_ind = adj_sparse_ind.to(device)
+adj_ind = adj_sparse_ind.to_dense()
+
+# Computing normalized Laplacian
+L_ind = aux_functions.compute_laplacian(adj_sparse_ind,num_nodes_ind)
+
+lam = eigvals[-1]
+L_aux = L_ind.cpu()
+k = 5
+m2 = 50
+
+s_vec = greedy(f, lam, L_aux, k, m2, exponent=0)
+s_vec = torch.tensor(s_vec)
+
+m3 = 4
+sampled_idx = []
+for i in range(m):
+    if s_vec[i] == 1:
+        if i < m-1:
+            idx = np.random.choice(np.arange(i*n_nodes_per_int,(i+1)*n_nodes_per_int),m3)
+        else:
+            if m3 > n_nodes_last_int:
+                m3 = n_nodes_last_int
+            idx = np.random.choice(np.arange(i*n_nodes_per_int,
+                                             i*n_nodes_per_int+n_nodes_last_int),m3)
+        idx = np.sort(idx)
+        sampled_idx += list(idx)
+
+graph_new = train_data.subgraph(torch.tensor(sampled_idx,device=device,dtype=torch.long))
+graph_new = graph_new.to(device)
+
+num_nodes_new = graph_new.x.shape[0]
+edge_index_new = graph_new.edge_index
+if graph_new.edge_weight is not None:
+    edge_weight_new = graph_new.edge_weight
+else:
+    edge_weight_new = torch.ones(edge_index_new.shape[1]).to(device)
+adj_sparse_new = torch.sparse_coo_tensor(edge_index_new, edge_weight_new, 
+                                         (num_nodes_new, num_nodes_new))
+adj_new = adj_sparse_new.to_dense()
+
+# Computing normalized Laplacian
+L_new = aux_functions.compute_laplacian(adj_sparse_new,num_nodes_new)
+
+K = 5
+eigvals_new, V_new = torch.lobpcg(L_new,k=K)
+V_new = V_new.type(torch.float32)
+V_rec = torch.zeros(num_nodes, K, device=device)
+
+for i in range(V_new.shape[1]):
+    v = V_new[:,i]
+    x0 = np.random.multivariate_normal(np.zeros(num_nodes),np.eye(num_nodes)/np.sqrt(num_nodes))
+    x0[sampled_idx] = v.cpu().numpy()*np.sqrt(m2*m3)/np.sqrt(num_nodes)
+    v_padded_lb = -torch.ones(num_nodes, device=device)
+    v_padded_lb[sampled_idx] = v*np.sqrt(m2*m3)/np.sqrt(num_nodes)
+    v_padded_ub = torch.ones(num_nodes, device=device)
+    v_padded_ub[sampled_idx] = v*np.sqrt(m2*m3)/np.sqrt(num_nodes)    
+    V_rec[:,i] = torch.from_numpy(reconstruct(f_rec, x0, v_padded_lb, v_padded_ub, L, k))
+
+pre_defined_kwargs = {'eigvecs': V_rec}
+
+train_data_new = Data(x=train_data.x, edge_index=train_data.edge_index,
+                      edge_label=train_data.edge_label,
+                      y=train_data.y,edge_label_index=train_data.edge_label_index,
+                      **pre_defined_kwargs)
+val_data_new = Data(x=val_data.x, edge_index=val_data.edge_index,
+                      edge_label=val_data.edge_label,
+                      y=train_data.y,edge_label_index=val_data.edge_label_index,
+                      **pre_defined_kwargs)
+test_data_new = Data(x=test_data.x, edge_index=test_data.edge_index,
+                      edge_label=test_data.edge_label,
+                      y=test_data.y,edge_label_index=test_data.edge_label_index,
+                      **pre_defined_kwargs)
+
+model = SignNetLinkPredNet(dataset.num_features+16*K, 32, 32, True, 1, 16, 16).to(device)
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.01)
+criterion = torch.nn.BCEWithLogitsLoss()
+model = train_link_predictor(model, train_data_new, val_data_new, optimizer, criterion)
+test_auc = eval_link_predictor(model, test_data_new)
+print(f"Test: {test_auc:.3f}")
+
+print()
