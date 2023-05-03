42c6fe316d7a6ec97387939ef02147eac56325dc
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-05-03 14:13:43 -0400

    Bug fix

--- vary_lr.sh
+++ vary_lr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python main_graphon.py vary_ratio 0.001 100 0.85 0.1 10 50 25 4 2
-python main_graphon.py vary_ratio 0.005 100 0.85 0.1 10 50 25 4 2
-python main_graphon.py vary_ratio 0.01 100 0.85 0.1 10 50 25 4 2
+python main_graphon.py vary_lr 0.001 100 0.85 0.1 10 50 25 4 2
+python main_graphon.py vary_lr 0.005 100 0.85 0.1 10 50 25 4 2
+python main_graphon.py vary_lr 0.01 100 0.85 0.1 10 50 25 4 2
