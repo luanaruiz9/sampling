20995422505a18b606c66d85d52cf937230be2dd
luanaruiz9 <luanaianararubiniruiz@gmail.com>
2023-05-05 09:45:02 -0400

    Added bash files

---
+++ vary_lr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python main_graphon.py vary_lr 0.001 100 0.85 0.1 10 50 25 4 2
+python main_graphon.py vary_lr 0.005 100 0.85 0.1 10 50 25 4 2
+python main_graphon.py vary_lr 0.01 100 0.85 0.1 10 50 25 4 2
