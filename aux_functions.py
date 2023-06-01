89268010588f950d541cf59ee921808ac91da54d
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-06-01 10:45:00 -0400

    Bug fix

--- aux_functions.py
+++ aux_functions.py
@@ -20,12 +20,19 @@ def compute_adj_from_data(data):
 
 def compute_degree(adj_sparse, num_nodes):
     device = adj_sparse.device
-    degree = torch.pow(torch.matmul(adj_sparse,torch.ones(num_nodes).to(device)),-0.5)
+    degree = torch.matmul(adj_sparse,torch.ones(num_nodes).to(device))
     edge_index_deg = torch.cat((torch.arange(num_nodes).unsqueeze(0),torch.arange(num_nodes).unsqueeze(0)),dim=0)
     degree_mx = torch.sparse_coo_tensor(edge_index_deg.to(device), degree, (num_nodes, num_nodes))
     return degree_mx
     
+def compute_inv_degree(adj_sparse, num_nodes):
+    device = adj_sparse.device
+    degree = torch.pow(torch.matmul(adj_sparse,torch.ones(num_nodes).to(device)),-0.5)
+    edge_index_deg = torch.cat((torch.arange(num_nodes).unsqueeze(0),torch.arange(num_nodes).unsqueeze(0)),dim=0)
+    degree_mx = torch.sparse_coo_tensor(edge_index_deg.to(device), degree, (num_nodes, num_nodes))
+    return degree_mx
+
 def compute_laplacian(adj_sparse, num_nodes):
-    degree_mx = compute_degree(adj_sparse,num_nodes)
+    degree_mx = compute_inv_degree(adj_sparse,num_nodes)
     L = torch.matmul(degree_mx,torch.matmul(adj_sparse,degree_mx))
     return L
