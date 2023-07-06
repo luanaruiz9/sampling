32b04e502c7901bae2d16f65dd2d77ad7139d9b4
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-07-06 14:54:18 -0700

    Bug fix

--- main_transf.py
+++ main_transf.py
@@ -112,8 +112,9 @@ D = aux_functions.compute_degree(adj_sparse, num_nodes)
 deg = torch.diagonal(D.to_dense()).squeeze()
 idx = torch.argsort(deg)
 idx = idx.to(device)
-graph = graph_og#.subgraph(idx)
-
+print(graph_og.edge_index)
+graph = graph_og.subgraph(idx)
+print(graph.edge_index)
 
 for r in range(n_realizations):
     
