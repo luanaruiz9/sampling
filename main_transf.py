a495b7f30472d06290d488089f3de0233a922f07
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-07-05 18:06:37 -0700

    Bug fix

--- main_transf.py
+++ main_transf.py
@@ -130,6 +130,7 @@ for r in range(n_realizations):
     D = aux_functions.compute_degree(adj_sparse, num_nodes)
     deg = torch.diagonal(D.to_dense()).squeeze()
     idx = torch.argsort(deg)
+    idx = idx.to(device)
     train_data = train_data.subgraph(idx)
     val_data = val_data.subgraph(idx)
     test_data = test_data.subgraph(idx)
