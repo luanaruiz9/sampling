7ff3d99d7b820203c828d12f22aba64e1c976d61
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-07-06 09:45:03 -0700

    Bug fix

--- main_transf.py
+++ main_transf.py
@@ -92,7 +92,7 @@ for r in range(n_realizations):
         dataset = Planetoid(root='/tmp/Cora', name='Cora', split='random')
         num_train_per_class = int((ratio_train*dataset[0].num_nodes)/dataset.num_classes)
         num_val = int(ratio_val*dataset[0].num_nodes)
-        num_test = dataset.num_nodes-num_val-num_train_per_class*dataset.num_classes
+        num_test = dataset[0].num_nodes-num_val-num_train_per_class*dataset.num_classes
         dataset = Planetoid(root='/tmp/Cora', name='Cora', split='random',
                             num_train_per_class=num_train_per_class, num_val=num_val, 
                             num_test=num_test)
@@ -100,7 +100,7 @@ for r in range(n_realizations):
         dataset = Planetoid(root='/tmp/CiteSeer', name='CiteSeer', split='random')
         num_train_per_class = int((ratio_train*dataset[0].num_nodes)/dataset.num_classes)
         num_val = int(ratio_val*dataset[0].num_nodes)
-        num_test = dataset.num_nodes-num_val-num_train_per_class*dataset.num_classes
+        num_test = dataset[0].num_nodes-num_val-num_train_per_class*dataset.num_classes
         dataset = Planetoid(root='/tmp/CiteSeer', name='CiteSeer', split='random',
                             num_train_per_class=num_train_per_class, num_val=num_val, 
                             num_test=num_test)
@@ -108,7 +108,7 @@ for r in range(n_realizations):
         dataset = Planetoid(root='/tmp/PubMed', name='PubMed', split='random')
         num_train_per_class = int((ratio_train*dataset[0].num_nodes)/dataset.num_classes)
         num_val = int(ratio_val*dataset[0].num_nodes)
-        num_test = dataset.num_nodes-num_val-num_train_per_class*dataset.num_classes
+        num_test = dataset[0].num_nodes-num_val-num_train_per_class*dataset.num_classes
         dataset = Planetoid(root='/tmp/PubMed', name='PubMed', split='random',
                             num_train_per_class=num_train_per_class, num_val=num_val, 
                             num_test=num_test)
