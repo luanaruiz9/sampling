94a76258bf3cd9febb4e5b0f86ab2b019cf08ef7
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-07-12 09:22:55 -0400

    Bug fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python main_link_pred.py cora 0.0013233812694319355 100 10 20 10 10 2 32 64
-python main_link_pred_wandb.py citeseer 0.004377884943245985 100 10 20 10 20 4 128 32
-python main_link_pred_wandb.py pubmed 0.00293072250746189 100 50 30 20 2 32 128
-python main_link_pred_wandb.py twitch-pt 0.00481447754363471 100 20 10 20 2 32 128
-python main_link_pred_wandb.py twitch-ru 0.0024717797601309043 100 20 10 10 2 64 64
+python main_link_pred.py citeseer 0.004377884943245985 100 10 20 10 20 4 128 32
+python main_link_pred.py pubmed 0.00293072250746189 100 50 30 20 2 32 128
+python main_link_pred.py twitch-pt 0.00481447754363471 100 20 10 20 2 32 128
+python main_link_pred.py twitch-ru 0.0024717797601309043 100 20 10 10 2 64 64
