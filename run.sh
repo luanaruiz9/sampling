b6672fccbcb8956b3f7cb24d4e307e66b864e191
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-07-13 14:23:05 -0400

    Another test

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python main_link_pred.py cora 0.001 100 10 50 25 20 1 32 128
-python main_link_pred.py citeseer 0.001 100 10 50 25 20 1 32 32
-python main_link_pred.py pubmed 0.003 100 10 50 25 50 3 32 128
-python main_link_pred.py twitch-pt 0.001 100 10 50 25 20 1 32 32
-python main_link_pred.py twitch-ru 0.003 100 10 50 25 20 1 32 128
+python main_link_pred.py cora 0.001 100 10 50 25 30 1 32 128
+python main_link_pred.py citeseer 0.001 100 10 50 25 30 1 32 32
+python main_link_pred.py pubmed 0.003 100 10 50 25 60 2 32 128
+python main_link_pred.py twitch-pt 0.001 100 10 50 25 30 1 32 32
+python main_link_pred.py twitch-ru 0.003 100 10 50 25 30 1 32 128
