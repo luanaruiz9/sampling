31f47f1049d415f573fbc6f642aa1294db9ff130
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-07-17 16:36:29 -0400

    Setting up run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-python main_link_pred.py cora 0.001 100 10 20 10 10 1 32 128
-python main_link_pred.py citeseer 0.001 100 10 20 10 10 1 32 32
-python main_link_pred.py pubmed 0.003 100 10 50 25 30 1 32 128
-python main_link_pred.py twitch-pt 0.001 100 10 20 10 10 1 32 32
-python main_link_pred.py twitch-ru 0.003 100 10 20 10 10 1 32 128
+
+python main_link_pred.py cora 0.001 100 10 20 10 20 1 32 32 20
+python main_link_pred.py citeseer 0.001 100 10 20 10 20 1 32 32 20
+python main_link_pred.py pubmed 0.001 100 10 50 25 30 1 32 32 20
+python main_transf.py cora 0.001 100 10 20 10 10 1 32 7
+python main_transf.py cora 0.001 100 10 20 10 20 1 32 7
+python main_transf.py cora 0.001 100 10 30 15 20 1 32 7
+python main_transf.py citeseer 0.001 100 10 20 10 10 1 32 6
+python main_transf.py citeseer 0.001 100 10 20 10 20 1 32 6
+python main_transf.py citeseer 0.001 100 10 30 15 20 1 32 6
+python main_transf.py pubmed 0.001 100 10 20 10 50 1 32 3
+python main_transf.py pubmed 0.001 100 10 20 10 20 1 32 3
+python main_transf.py pubmed 0.001 100 10 30 15 20 1 32 3
