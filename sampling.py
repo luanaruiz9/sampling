958a42ab479ae1abe26e5631a563f5871d0efcba
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-07-19 18:00:56 -0400

    Test

--- sampling.py
+++ sampling.py
@@ -124,8 +124,8 @@ def greedy(f, lam, L, k, m, exponent=10000000): # m is sampling set size
             amax = idx_x[np.argmax(phi)]
         s_vec[amax] = 1
         idx_x = idx_x[s_vec[idx_x]==0]
-        if res.success and res.fun > lam:
-            break
+        #if res.success and res.fun > lam:
+            #break
     return s_vec, n_iters
 
 ##################################
