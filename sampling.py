a69b22070ab42a13f3fa73c28c5fcdae0d0823a5
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-07-19 12:44:05 -0400

    Test

--- sampling.py
+++ sampling.py
@@ -108,8 +108,8 @@ def greedy(f, lam, L, k, m, exponent=5): # m is sampling set size
             #x0 = np.ones(n-i)/(n-i)
             #x0 = np.zeros(n-i)
             #x0[np.random.choice(n-i)]=1
-            res = opt.minimize(f, x0, args=(lam, L, k, s_vec),
-                               options={'disp': True}) # method='nelder-mead',
+            res = opt.minimize(f, x0, args=(lam, L, k, s_vec), method='Newton-CG',
+                               options={'disp': True})
             n_iters += res.nit
             #res = opt.minimize(f_lobpcg, np.expand_dims(x0,axis=1), args=(lam, L, k, s_vec),
             #                   options={'disp': True,'maxiter' : 10})
