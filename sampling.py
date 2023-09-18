1c56187d203ad82d0bd7058b42bf09c88acb4c31
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-09-18 17:35:12 -0400

    Bug fix

--- sampling.py
+++ sampling.py
@@ -183,7 +183,7 @@ def cluster_hk_pr(A, seed, cheeger, eps, sz, vol_den):
     for i, node in enumerate(list(idx)):
         S.append(node)
         volS = torch.sum(deg_vec[S]).cpu().numpy()
-        if len(A[S].shape) == 1:
+        if len(torch.argwhere(A[S]).shape) <= 1:
             bS = list(torch.argwhere(A[S]).cpu().numpy())
         else:
             bS = list(torch.argwhere(A[S])[1].cpu().numpy())
